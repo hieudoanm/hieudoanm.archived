8b87c9fdb3cfcf069edfd93451e3ef4a0b9262d4
hieudoanm <hieumdoan@gmail.com>
2023-11-04 15:58:19 +0700

    Add Chess Web

---
+++ projects/chess.com/web/src/common/utils/resolve-query/index.ts
@@ -0,0 +1,12 @@
+import { ParsedUrlQuery } from 'node:querystring';
+
+export const resolveQuery = <T>(
+  query: ParsedUrlQuery,
+  key: string,
+  fallback: string = ''
+): string => {
+  const value: string | string[] | undefined = query[key];
+  if (!value) return fallback;
+  if (typeof value === 'string') return value;
+  return value[0];
+};
---
+++ projects/chess.com/web/src/layout/__tests__/index.test.tsx
@@ -0,0 +1,22 @@
+import { render } from '@testing-library/react';
+import { Layout } from '..';
+import { UserProvider } from '@auth0/nextjs-auth0/client';
+
+jest.mock('next/router', () => ({
+  useRouter: jest
+    .fn()
+    .mockReturnValue({ asPath: '', events: { on: jest.fn(), off: jest.fn() } }),
+}));
+
+describe('Layout', () => {
+  it('to match snapshot', () => {
+    const { container } = render(
+      <UserProvider>
+        <Layout>
+          <></>
+        </Layout>
+      </UserProvider>
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
