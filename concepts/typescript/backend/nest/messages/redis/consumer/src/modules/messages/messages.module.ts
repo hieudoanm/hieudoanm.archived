453799b70f07aa345db9be00c6ca68b886801aba
hieudoanm <hieumdoan@gmail.com>
2023-12-29 11:20:43 +0700

    Merge all data

--- concepts/typescript/backend/nest/messages/redis/consumer/src/modules/messages/messages.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RedisModule } from './redis/redis.module';
-import { RabbitModule } from './rabbitmq/rabbitmq.module';
-
-@Module({
-  imports: [RabbitModule, RedisModule],
-})
-export class MessagesModule {}
--- concepts/typescript/backend/nest/messages/redis/producer/src/modules/messages/messages.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RedisModule } from './redis/redis.module';
-import { RabbitModule } from './rabbitmq/rabbitmq.module';
-import { KafkaModule } from './kafka/kafka.module';
-
-@Module({
-  imports: [KafkaModule, RabbitModule, RedisModule],
-})
-export class MessagesModule {}
