1946eab92222da9061f2fe0c76e5e1fbba7c6854
hieudoanm <hieumdoan@gmail.com>
2023-12-29 11:22:46 +0700

    Remove nest grpc

--- concepts/typescript/backend/nest/services/grpc/src/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { HealthModule } from './modules/health/health.module';
-import { HeroesModule } from './modules/heroes/heroes.module';
-
-@Module({
-  imports: [HealthModule, HeroesModule],
-})
-export class AppModule {}
