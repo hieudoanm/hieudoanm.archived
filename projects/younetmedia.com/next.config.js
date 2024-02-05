4cbc9f0f59d270306c1b67dbe3e7f61bcae7b7ea
hieudoanm <hieumdoan@gmail.com>
2024-02-05 18:57:21 +0700

    UPdate next.config.js

--- projects/younetmedia.com/next.config.js
+++ projects/younetmedia.com/next.config.js
@@ -6,8 +6,8 @@ console.log(`NODE_ENV=${NODE_ENV}`);
 const withPWA = require('next-pwa')({
   dest: 'public',
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-  register: process.env.NODE_ENV !== 'development',
+  disable: NODE_ENV === 'development',
+  register: NODE_ENV !== 'development',
 });
 
 const nextConfig = {
