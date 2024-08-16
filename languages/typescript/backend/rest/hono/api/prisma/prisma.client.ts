0529cafe99ef4a253523cab5025c00ba5cce8401
hieudoanm <hieumdoan@gmail.com>
2024-08-16 10:14:57 +0700

    Fix hono

--- languages/typescript/backend/rest/hono/api/prisma/prisma.client.ts
+++ languages/typescript/backend/rest/hono/api/prisma/prisma.client.ts
@@ -1,4 +1,3 @@
 import { PrismaClient } from '@prisma/client/edge';
-import { withAccelerate } from '@prisma/extension-accelerate';
 
-export const prismaClient = new PrismaClient().$extends(withAccelerate());
+export const prismaClient = new PrismaClient();
