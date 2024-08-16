2f02f274b552a2d38ae5a162ae67ef8ea4366a8e
hieudoanm <hieumdoan@gmail.com>
2024-08-16 16:54:31 +0700

    Fix Scripts

--- languages/typescript/apps/web/scripts/sh/print-env.sh
+++ languages/typescript/apps/web/scripts/sh/print-env.sh
@@ -1,3 +1,4 @@
-lines=$(printenv)
-IFS=$'\n' sorted=($(sort <<< "${lines[*]}")); unset IFS
-printf "[%s]\n" "${sorted[@]}"
+lines=$(printenv);
+IFS=$'\n' sorted=$(sort <<< "${lines[*]}");
+unset IFS;
+printf "%s" "${sorted[@]}";
