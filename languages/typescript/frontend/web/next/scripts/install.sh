540fabd398bb600703c8005fd30300b159ee630e
hieudoanm <hieumdoan@gmail.com>
2024-08-24 09:33:28 +0700

    Update set-up.sh

--- languages/typescript/frontend/web/next/scripts/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-pnpm add --exact react react-dom next-pwa
-pnpm add @trpc/server@next @trpc/client@next @trpc/react-query@next @trpc/next@next @tanstack/react-query@latest zod
-pnpm add --dev --exact prisma prisma-dbml-generator prisma-json-schema-generator prisma-erd-generator @mermaid-js/mermaid-cli prisma-trpc-generator prisma-markdown
-pnpm add --dev --exact @million/lint @testing-library/react @types/jest @types/react @types/react-dom daisyui eslint-plugin-github eslint-plugin-jest eslint-plugin-prettier eslint-plugin-sonarjs husky jest jest-environment-jsdom kill-port million prettier prettier-plugin-tailwindcss rimraf ts-jest ts-node typescript
