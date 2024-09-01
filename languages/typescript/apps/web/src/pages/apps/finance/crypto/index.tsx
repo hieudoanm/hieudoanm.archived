9c22f4577a32cc7918e24a9de22d4c1a6b12f33d
hieudoanm <hieumdoan@gmail.com>
2024-09-02 01:06:16 +0700

    Fix Build

--- languages/typescript/apps/web/src/pages/apps/finance/crypto/index.tsx
+++ languages/typescript/apps/web/src/pages/apps/finance/crypto/index.tsx
@@ -2,7 +2,7 @@ import { GridTemplate } from '@web/router/apps/Apps';
 import { NextPage } from 'next';
 
 const CryptoApps: NextPage = () => {
-  return <GridTemplate full folder='crypto' />;
+  return <GridTemplate full folder='finance-crypto' />;
 };
 
 export default CryptoApps;
