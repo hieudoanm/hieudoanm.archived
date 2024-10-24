c060242671e238042dff001ee127f128a3bfb97e
hieudoanm <hieumdoan@gmail.com>
2024-10-24 20:21:42 +0700

    Update Fix

--- languages/typescript/backend/graphql/express/src/server.ts
+++ languages/typescript/backend/graphql/express/src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { createHandler } from 'graphql-http/lib/use/express';
 import { buildSchema } from 'graphql';
 import { ruruHTML } from 'ruru/server';
@@ -22,9 +22,11 @@ const PORT: string = process.env.PORT ?? '4000';
 const main = () => {
   const app = express();
 
-  app.get('/', (_req, res) => {
-    res.type('html');
-    res.end(ruruHTML({ endpoint: '/graphql' }));
+  app.get('/', (request: Request, response: Response) => {
+    const { query } = request;
+    console.log(query);
+    response.type('html');
+    response.end(ruruHTML({ endpoint: '/graphql' }));
   });
 
   app.all(
