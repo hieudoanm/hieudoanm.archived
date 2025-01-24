f191f91b54449b776be2216167ee0e1ff3b20955
hieudoanm <hieumdoan@gmail.com>
2025-01-24 16:32:34 +0700

    Remove Weaether and Crypto

--- src/pages/apps/clock/index.tsx
+++
@@ -1,8 +0,0 @@
-import { GridTemplate } from '@web/router/apps/Apps';
-import { NextPage } from 'next';
-
-const ClockApps: NextPage = () => {
-  return <GridTemplate folder='clock' />;
-};
-
-export default ClockApps;
