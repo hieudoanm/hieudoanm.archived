b175ebfcef3ca13d34d4c062e1df9a3c50975d40
hieudoanm <hieumdoan@gmail.com>
2025-01-24 11:04:39 +0700

    Update App

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,8 @@
 import type { Config } from 'tailwindcss';
-import daisyui from 'daisyui';
 
 const config: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: { extend: {} },
-  plugins: [daisyui as any],
 };
+
 export default config;
