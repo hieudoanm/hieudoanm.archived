c70882bcc6459a0452fc6d56e25ddae46cf9f932
hieudoanm <hieumdoan@gmail.com>
2025-01-26 13:25:28 +0700

    Update Apps

--- src/pages/apps/csv/index.tsx
+++
@@ -1,8 +0,0 @@
-import { GridTemplate } from '@web/router/apps/Apps';
-import { NextPage } from 'next';
-
-const CsvApps: NextPage = () => {
-  return <GridTemplate folder='csv' />;
-};
-
-export default CsvApps;
